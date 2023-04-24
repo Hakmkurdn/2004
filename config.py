aad42bb0b60a21e565bd28f4afb6e276d87a4eb1
Hakmkurdn <125227349+Hakmkurdn@users.noreply.github.com>
2023-04-24 19:23:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,26 +5,26 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578"))
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")
-BOT_TOKEN = getenv("BOT_TOKEN", "5385370603:AAHjSbZFL1OLl_uOm3v8lS0iKSYhRJS_bKs")
-SESSION_NAME = getenv("SESSION_NAME", "AgAGFRW8AN9biZhAIB_X7rubbTCwNtc48oa-geXlaVsQbV9M1emAEV_iGPw_W-lNP1HGbOuQgF9NAUIxlp8-6rgN16-7Upuhst2wCxewyx0VP7Cxb5k_9EXap6_xeU2v4So9RZMt95XYxhGc4hWO_ikd9P46iHLMjXW8GjQAnGvY-bIrM6XqIXF1yeC3vixEa8knOHGkedZU3FYyLhEILy8TV6eH5jOGvsyw5o7xFElb2zZId_vlYO--B3xSWIMUR6zyH8wozehe9tIn0jZptYjY1rqWOMoYS9wtqvBThVazL60nrgL-KdikJC9beDJLLR0C-wzuAtARPCZ-JK6oG-gVAAAAAUM3evYA")
+API_ID = int(getenv("API_ID", "24325615"))
+API_HASH = getenv("API_HASH", "be5835a24d0cdaedc1cd256def4f9957")
+BOT_TOKEN = getenv("BOT_TOKEN", "5497887119:AAEACkD_ckbUPBlgnwgVk0wZ5Ufpu9phDW4")
+SESSION_NAME = getenv("SESSION_NAME", "BABAWUvUhIkHOcV6iBjqCF3GzZst-X5NY5tAAEvVQ6iBgUjF2lDy3JZ-N-NdNebk2sdTOjzaOSaZ8JCHv0EuP6VvI2ZMTON_fizt1zzZemaHRWZ6WJBrl65t-DCNEFWGn7nMSsY-SKBC9YK0rFkRrK3Glvs1eTz-4HbFTrlXdQd_dOQQjxl88_MsFzmAlkVutrMXvwl_-FRiymh_uTlqqzv011VyGPor93sg_JpU0iYtSBlnzmMUt2PWafOuGT8P8PzT7IcmpplWtNrF198huwvu30EG85EAZ0R6Nk0qCKhPrxlGOWFj8O-E9fZWejZ9I1_v5-xsuvv7EVvE_gWam-_UAAAAAUZPwtIA")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "c_l_h")
 ALIVE_NAME = getenv("ALIVE_NAME", "song")
-BOT_USERNAME = getenv("BOT_USERNAME", "W7MBoT")
+BOT_USERNAME = getenv("BOT_USERNAME", "Gotcar_bot")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/STKR2/2004")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "c_l_h")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "w_1_4")
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5355696037").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5474599634").split()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
